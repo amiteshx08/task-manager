abd8af2304910c84b2a6423e5b892800318865ab
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2025-12-24 18:33:13 +0530

    Corrections made

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const taskForm = document.querySelector('#task-form');
-const taskList = document.querySelector('#task-list');
+const taskForm = document.querySelector("#task-form");
+const taskList = document.querySelector("#task-list");
 
-console.log(taskForm, taskList);
+const marker = '-';
 
-taskForm.addEventListener('submit', function(e){
-    e.preventDefault();
+taskForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const taskInput = document.querySelector('#form-input');
-    const taskText = taskInput.value.trim();
+  const taskInput = document.querySelector('#task-input');
+  const taskText = taskInput.value.trim();
 
-    if(taskText !== ""){
+  if(taskText !== "") {
 
-        
-    }
-})
+    const taskItem = document.createElement('li');
+    const taskName = document.createElement('span');
+    const deleteItem = document.createElement('span');
+    taskItem.classList.add('task-item');
+    taskName.classList.add('task-name');
+    deleteItem.classList.add('delete-item');
+
+    taskName.textContent = `${marker} ${taskText}`;
+    deleteItem.textContent = 'del';
+
+    taskItem.addEventListener('click', function() {
+        console.log('completed');
+        this.classList.toggle('completed');
+    })
+    taskItem.appendChild(taskName);
+    taskItem.appendChild(deleteItem);
+
+    taskList.appendChild(taskItem);
+    
+    taskInput.value = "";
+  }
+
+});
+
